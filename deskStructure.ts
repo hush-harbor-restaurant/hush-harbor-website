5134277095f9e4f6deb6750fa58f9f8b5c848fcb
Jack Nugent <jnugent@costar.com>
2025-07-08 15:20:50 -0400

    Fix structure add other ones

--- deskStructure.ts
+++ deskStructure.ts
@@ -1,12 +1,22 @@
 import type { StructureBuilder } from "sanity/structure";
 
-export default (S: StructureBuilder) =>
-  S.list()
+export default (S: StructureBuilder) => {
+  // Filter out the 'eventsImage' type from the default list
+  const defaultItems = S.documentTypeListItems().filter(
+    (item) => item.getId() !== "eventsImage"
+  );
+
+  return S.list()
     .title("Content")
     .items([
+      // Your singleton item
       S.listItem()
         .title("Events Image")
         .child(
           S.document().schemaType("eventsImage").documentId("eventsImage")
         ),
+
+      // All other document types
+      ...defaultItems,
     ]);
+};
