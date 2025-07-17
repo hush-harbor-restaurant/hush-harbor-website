45d2b8d26ac77c75dfdd1ceb6865f70bf2304b43
Jack Nugent <jnugent@costar.com>
2025-07-17 11:23:16 -0400

    Add astro config fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import netlify from "@astrojs/netlify";
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
