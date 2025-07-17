e02915059ca35653e19c9345141a8e6ed758866e
Jack Nugent <jnugent@costar.com>
2025-07-17 09:26:46 -0400

    Remove events page from main page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useLiveScrollY } from "../hooks/useLiveScrollY";
+import clsx from "clsx";
 import { useActiveSection } from "../hooks/useActiveSection";
 
 const sectionBgClasses: Record<string, string> = {
@@ -12,32 +13,42 @@ const sectionBgClasses: Record<string, string> = {
 const navLinks = [
   { name: "Info", href: "#info" },
   { name: "Menu", href: "#menu" },
-  { name: "Events", href: "#events" },
 ];
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const scrollY = useLiveScrollY();
-  const activeSection =
-    useActiveSection(["home", "info", "menu", "events"]) ?? "home";
+  const activeSection = useActiveSection(["home", "info", "menu"]) ?? "home";
 
   const isSticky = scrollY > 10;
 
   return (
     <nav
-      className={`text-xl fixed w-full z-50 text-stone-100 transition-colors duration-200
-  max-md:bottom-0
-  ${isSticky ? "md:top-0" : "md:bottom-0"}
-  md:flex md:justify-between
-  ${sectionBgClasses[activeSection]} ${!isSticky ? "md:bg-transparent" : ""}`}
+      className={clsx(
+        "text-xl fixed w-full z-50 text-stone-100 transition-colors duration-200",
+        "max-md:bottom-0",
+        {
+          "md:top-0": isSticky,
+          "md:bottom-0": !isSticky,
+          "md:flex md:justify-between": true,
+          [sectionBgClasses[activeSection]]: true,
+          "md:bg-transparent": !isSticky,
+        }
+      )}
     >
       <div
         className={`flex justify-between items-center max-mod:px-4 max-md:py-3 max-md:${sectionBgClasses[activeSection]}`}
       >
         <div
-          className={`text-xl font-bold text-stone-300 ${!isSticky ? "bg-leafy-foreground" : ""} md:${sectionBgClasses[activeSection]} px-5 md:px-8 py-2 ${
-            isSticky ? "md:rounded-br-md" : "md:rounded-tr-md"
-          }`}
+          className={clsx(
+            "text-xl font-bold text-stone-300",
+            {
+              "bg-leafy-foreground md:rounded-tr-md": !isSticky,
+              "md:rounded-br-md": isSticky,
+            },
+            "px-5 md:px-8 py-2",
+            `md:${sectionBgClasses[activeSection]}`
+          )}
         >
           Hush Harbor
         </div>
@@ -61,11 +72,19 @@ const Navbar = () => {
       </div>
 
       <ul
-        className={`${
-          isOpen ? "block" : "hidden"
-        } md:flex md:flex-row md:space-x-6 px-5 md:px-8 py-1 max-md:pb-2 ${sectionBgClasses[activeSection]} flex-col md:flex md:items-center transition-colors duration-200 ${
-          isSticky ? "md:rounded-bl-md" : "md:rounded-tl-md"
-        }`}
+        className={clsx(
+          {
+            block: isOpen,
+            hidden: !isOpen,
+            "md:rounded-bl-md": isSticky,
+            "md:rounded-tl-md": !isSticky,
+          },
+          "md:flex md:flex-row md:space-x-6",
+          "px-5 md:px-8 py-1 max-md:pb-2",
+          "flex-col md:items-center",
+          "transition-colors duration-200",
+          sectionBgClasses[activeSection]
+        )}
       >
         {navLinks.map((link) => (
           <li key={link.name}>
