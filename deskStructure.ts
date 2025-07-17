07151c83491f86fc623d8c7852ccabaa3d14ad74
Jack Nugent <jnugent@costar.com>
2025-07-17 16:24:21 -0400

    Remove rich text

--- deskStructure.ts
+++ deskStructure.ts
@@ -3,15 +3,12 @@ import type { StructureBuilder } from "sanity/structure";
 
 export default (S: StructureBuilder) => {
   const defaultItems = S.documentTypeListItems().filter(
-    (item) => !["eventsImage", "infoText"].includes(item.getId()!),
+    (item) => !["eventsImage"].includes(item.getId()!),
   );
 
   return S.list()
     .title("Content")
     .items([
-      S.listItem()
-        .title("Info Text")
-        .child(S.document().schemaType("infoText").documentId("infoText")),
       S.listItem()
         .title("Events Image")
         .child(
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,5 @@
 import menuItem from "./menuItem";
 import menuSection from "./menuSection";
 import eventsImage from "./eventsImage";
-import infoText from "./infoText";
 
-export const schemaTypes = [menuItem, menuSection, eventsImage, infoText];
+export const schemaTypes = [menuItem, menuSection, eventsImage];
